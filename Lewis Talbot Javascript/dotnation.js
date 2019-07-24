ba0f773ccb86b7a3ad11a03b0d8607f790f1ada0
codenation <codenation@codenations-MacBook-Pro.local>
2019-07-24 10:29:34 +0100

    This considered a comment

---
+++ Lewis Talbot Javascript/dotnation.js
@@ -0,0 +1,22 @@
+/*
+//console.log("hello". length);
+//.length counts the character in a string.
+
+console.log("Hello". toUpperCase());
+
+*/
+console.log(Math.random()*10)
+
+console.log(Math.floor(Math.random()*10));
+
+console.log("      |     |      ");
+console.log("      |     |      ");
+console.log("      |     |      ");
+console.log("-------------------");
+console.log("      |     |      ");
+console.log("      |     |      ");
+console.log("      |     |      ");
+console.log("-------------------");
+console.log("      |     |      ");
+console.log("      |     |      ");
+console.log("      |     |      ");
